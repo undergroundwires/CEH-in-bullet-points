ff154a128be85cd32d2adf82181ac4a0757ae0c7
Oleh Prypin <oleh@pryp.in>
2021-09-17 14:46:46 +0200

    Revert readme, rewrite on the fly instead

--- gen_index.py
+++ gen_index.py
@@ -1,8 +1,10 @@
-# Moves the "README.md" file into chapters/, for use by MkDocs.
-
 import re
 import mkdocs_gen_files
 
+# Move the "README.md" file into chapters/, for use by MkDocs.
 with open("README.md") as in_f, mkdocs_gen_files.open("README.md", "w") as out_f:
     for line in in_f:
-        out_f.write(re.sub(r'\bchapters/', '', line))
+        line = re.sub(r'\bchapters/', '', line)
+        # Replace 3-space indentation in lists with 4-space indentation:
+        line = re.sub(r'^ ?( +)\1\1\b', r'\1\1\1\1', line)
+        out_f.write(line)
